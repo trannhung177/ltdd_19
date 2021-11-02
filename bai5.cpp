23980ce1f3dab880799ba8118613043ea8f79f5c
trannhung177 <93437189+trannhung177@users.noreply.github.com>
2021-11-02 21:08:34 +0700

    Add files via upload

---
+++ bai5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<iomanip.h>
+using namespace std;
+int main()
+{
+	float a,b,c;
+	cout<<"nhap diem chuyen can:";cin>>a;
+	cout<<"nhap diem dieu kian:";cin>>b;
+	cout<<"nhap diem cuoi ky:";cin>>c;
+	float s=a*0.1+b*0.3+c*0.6;
+	cout<<"dien tb la:"<<s;
+	return 0;
+	
+}
